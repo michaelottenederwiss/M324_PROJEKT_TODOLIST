27056edc3b63687b3ee0e667cd009ca084e39213
michaelottenederwiss <159884793+michaelottenederwiss@users.noreply.github.com>
2024-06-28 21:09:13 +0200

    im fairly optimistic that there is non-zero probability that his might actually fix the test

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -5,6 +5,7 @@ test('renders learn react link', async () => {
   render(<App />);
   console.log(document.body.innerHTML); // Log the rendered output
 
-  const linkElement = await screen.findByText(/learn react/i);
+  const linkElement = await screen.findByText(/ToDo Liste/i);
+  
   expect(linkElement).toBeInTheDocument();
 });
