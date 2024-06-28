4e8f07a5fff0747e2e07a339ca418ecf25c1d81b
michaelottenederwiss <159884793+michaelottenederwiss@users.noreply.github.com>
2024-06-28 21:06:20 +0200

    trying once again to fix the test

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders learn react link', async () => {
   render(<App />);
+  console.log(document.body.innerHTML); // Log the rendered output
+
   const linkElement = await screen.findByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
